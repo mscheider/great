8b516b8e154e47b3e41a6a3fd7bb555d16a1c1fc
Marcelo Nicolas Scheider <marce_scheider@hotmail.com>
2022-05-16 22:25:56 -0300

    menu e-commerce

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <nav>
+            <h1 className='titulo'> Great!</h1>
+            <div className='container'>
+                <button className=''>Remeras</button>
+                <button>Tops</button>
+                <button>Calzas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar   
