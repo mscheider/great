826097f7beeaa7607ec8e5c8638b2d999289e047
Marcelo Nicolas Scheider <marce_scheider@hotmail.com>
2022-05-21 15:11:55 -0300

    clase 2 de componentes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <NavBar />
         <ItemListContainer greeting="Hola Coders"/>
-      </header>
+        <ItemCount />
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const ItemCount = () => {
+    const [count,setCount] = useState(0)
+
+    const decrement = () => {
+        setCount((count) => count - 1)
+    }
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    return(
+        <div style={{ display: 'Block'}}>
+            <button onClick={decrement}>-</button>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+        </div>
+    )
+
+}
+
+export default ItemCount
